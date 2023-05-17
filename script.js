1e897b9f4e93fc5f29f6fdc0a8ce4d16174fc31c
JMarioGilD <jmariogild88@gmail.com>
2023-05-16 21:31:02 -0500

    creación del codigo lista de contactos

---
+++ script.js
@@ -0,0 +1,30 @@
+/* 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido 
+      como una sola cadena de caracteres. */
+      let contactos = ['Steven Strange', 'Wanda Maximoff', 'Zatanna Zatara', 'Kent Nelson', 'Willow Rosenberg'];
+
+      /* 2. Crea una función para añadir un nuevo contacto a una lista */
+      function agregarContacto(contacto){
+          contactos.push(contacto);
+      }
+      
+      agregarContacto('Rachel Roth');
+      
+      /* 3. Crea una función para borrar un contacto existente de la lista */
+      let eliminarContacto = function(contacto){
+          let indice = contactos.indexOf(contacto);
+          contactos.splice(indice, 1)
+      }
+      
+      eliminarContacto('Kent Nelson')
+      
+      console.log(contactos)
+      
+      /* 4. Crea una función para imprimir en consola los contactos presentes en la lista */
+      let mostrarContactos = () => {
+          contactos.sort();
+          for (let i = 0; i < contactos.length; i++) {
+              console.log(contactos[i]);
+          }
+      }
+      
+      mostrarContactos()
